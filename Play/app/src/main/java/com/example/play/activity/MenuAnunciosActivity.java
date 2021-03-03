d141c6701b84e304773a84633a8449202118e8ba
Robson <robsondejesus1996@hotmail.com>
2021-03-03 19:54:01 -0300

    Adicionado a funcionalidade de listar meus anúncios, na tela de meus anúncios.

--- Play/app/src/main/java/com/example/play/activity/MenuAnunciosActivity.java
+++ Play/app/src/main/java/com/example/play/activity/MenuAnunciosActivity.java
@@ -4,18 +4,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.play.R;
+import com.example.play.adapter.AdapterAnuncios;
+import com.example.play.helper.ConfiguracaoFirebase;
+import com.example.play.model.Anuncio;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MenuAnunciosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclearAnuncios;
+    private List<Anuncio> anuncios = new ArrayList<>();
+    private AdapterAnuncios adapterAnuncios;
+    private DatabaseReference anuncioUsuarioRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_anuncios);
+        //configurações iniciais
+        anuncioUsuarioRef = ConfiguracaoFirebase.getFirebase()
+                .child("meus_anuncios")
+                .child(ConfiguracaoFirebase.getIdUsuario());
+
+        inicializarComponentes();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,5 +53,42 @@ public class MenuAnunciosActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //configurar RecyclearView
+        recyclearAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclearAnuncios.setHasFixedSize(true);
+        adapterAnuncios = new AdapterAnuncios(anuncios, this);
+        recyclearAnuncios.setAdapter(adapterAnuncios);
+
+        //recuperar anúncios para o usuário
+        recuperarAnuncio();
+
+    }
+
+    private void recuperarAnuncio(){
+
+       anuncioUsuarioRef.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+               anuncios.clear();
+
+               for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    anuncios.add(ds.getValue(Anuncio.class));
+               }
+               Collections.reverse(anuncios);
+               adapterAnuncios.notifyDataSetChanged();
+
+           }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError error) {
+
+           }
+       });
+
+    }
+
+    public void inicializarComponentes(){
+        recyclearAnuncios = findViewById(R.id.recyclearAnuncios);
     }
 }
